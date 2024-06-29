5ea2f15374bc132650ad369fd2e8bcd5d3738a0a
Nate Schainblatt <nschainblatt@gmail.com>
2024-06-29 17:40:45 -0400

    Optimized copied solution from day 3 part 1 for day 3 part 2, implemented test in day 3 part 2 to ensure output is the same

--- src/com/aoc/day3/Day3Part2.java
+++ src/com/aoc/day3/Day3Part2.java
@@ -1,8 +1,5 @@
 package com.aoc.day3;
 
-import java.util.List;
-import java.util.ArrayList;
-
 import com.aoc.api.AdventOfCodeApi;
 import com.aoc.Day;
 
@@ -16,273 +13,106 @@ public class Day3Part2 implements Day {
   public int solve() {
     String[] lines = api.getInput(2023, 3);
     int sum = 0;
-    ParsedLine previousParsedLine = null;
 
     for (int i = 0; i < lines.length; i++) {
-      ParsedLine parsedLine = new ParsedLine(i + 1, lines[i]);
-      List<Number> numberLocations = parsedLine.parseNumberLocations();
-      List<Symbol> symbolLocations = parsedLine.parseSymbolLocations();
-      parsedLine.setNumberLocations(numberLocations);
-      parsedLine.setSymbolLocations(symbolLocations);
-      int partialSum = parsedLine.getEnginePartSumFromCurrentLine();
-
-      if (i != 0 && previousParsedLine != null) {
-        partialSum += parsedLine.getEnginePartSumFromPreviousLine(previousParsedLine);
-      }
-
-      sum += partialSum;
-      previousParsedLine = parsedLine;
+      sum += getEnginePartSumFromLine(i, lines[i], lines);
     }
 
     return sum;
   }
 
-  public static <T> boolean contains(T[] array, T element) {
-    for (T t : array) {
-      if (t.equals(element)) {
+  private boolean isEnginePartNumber(String line, String[] lines, int row, int startingIndex, int endingIndex, int maxXIndex,
+      int maxYIndex) {
+    // Check horizontal (left + right)
+    if (startingIndex > 0) { // Can check left
+      if (isSymbol(line.charAt(startingIndex - 1))) {
         return true;
+      }
+    }
+    if (endingIndex < maxXIndex) { // Can check right
+      if (isSymbol(line.charAt(endingIndex + 1))) {
+        return true;
+      }
+    }
+
+    // Check vertical (above + below + diagonal)
+    if (row > 0) { // Can check above
+      int startingAboveIndex = startingIndex;
+      int endingAboveIndex = endingIndex;
+      String previousLine = lines[row - 1];
+      if (startingAboveIndex > 0) { // Can check left above diagonal
+        startingAboveIndex -= 1;
+      }
+      if (endingAboveIndex < maxXIndex) { // Can check right above diagonal
+        endingAboveIndex += 1;
+      }
+      // Loop over valid above indeces
+      for (int i = startingAboveIndex; i <= endingAboveIndex; i++) {
+        if (isSymbol(previousLine.charAt(i))) {
+          return true;
+        }
+      }
+    }
+    if (row < maxYIndex) { // Can check below
+      int startingBelowIndex = startingIndex;
+      int endingBelowIndex = endingIndex;
+      String nextLine = lines[row + 1];
+      if (startingBelowIndex > 0) { // Can check left below diagonal
+        startingBelowIndex -= 1;
+      }
+      if (endingBelowIndex < maxXIndex) { // Can check right below diagonal
+        endingBelowIndex += 1;
+      }
+      // Loop over valid below indeces
+      for (int i = startingBelowIndex; i <= endingBelowIndex; i++) {
+        if (isSymbol(nextLine.charAt(i))) {
+          return true;
+        }
       }
     }
     return false;
   }
 
-  public static class ParsedLine {
-    final int number;
-    final String line;
-    private List<Number> numberLocations;
-    private List<Symbol> symbolLocations;
+  public int getEnginePartSumFromLine(int row, String line, String[] lines) {
+    int partialSum = 0;
+    boolean inDigit = false;
+    int digitStartingIndex = -1;
+    Character previousCharacter = null;
+    char[] characters = line.toCharArray();
+    StringBuilder digitBuilder = new StringBuilder("");
 
-    public List<Number> getNumberLocations() {
-      return this.numberLocations;
-    }
-
-    public List<Symbol> getSymbolLocations() {
-      return this.symbolLocations;
-    }
-
-    public void setNumberLocations(List<Number> numberLocations) {
-      this.numberLocations = numberLocations;
-    }
-
-    public void setSymbolLocations(List<Symbol> symbolLocations) {
-      this.symbolLocations = symbolLocations;
-    }
-
-    public ParsedLine(int number, String line) {
-      this.number = number;
-      this.line = line;
-      this.numberLocations = new ArrayList<>();
-      this.symbolLocations = new ArrayList<>();
-    }
-
-    public int getEnginePartSumFromCurrentLine() {
-      int sum = 0;
-      for (Number number : this.numberLocations) {
-        int possibleLeftIndex = number.startingIndex - 1;
-        int possibleRightIndex = number.endingIndex + 1;
-        for (Symbol symbol : this.symbolLocations) {
-          if (symbol.index > possibleRightIndex) {
-            break;
-          }
-          if ((symbol.index == possibleLeftIndex || symbol.index == possibleRightIndex)
-              && !number.isEnginePart()) {
-            sum += number.value;
-            number.setIsEnginePart(true);
-          }
+    for (int i = 0; i < characters.length; i++) {
+      if (Character.isDigit(characters[i])) {
+        if (previousCharacter == null || !Character.isDigit(previousCharacter)) {
+          digitStartingIndex = i;
         }
-      }
-      return sum;
-    }
-
-    public int getEnginePartSumFromPreviousLine(ParsedLine previousParsedLine) {
-      int sum = 0;
-      sum += ParsedLine.checkVerticalAndDiagonal(previousParsedLine, this);
-      sum += ParsedLine.checkVerticalAndDiagonal(this, previousParsedLine);
-      return sum;
-    }
-
-    private static int checkVerticalAndDiagonal(ParsedLine parsedLineA, ParsedLine parsedLineB) {
-      int sum = 0;
-      for (Number previousNumber : parsedLineA.getNumberLocations()) {
-        int lineLengthA = parsedLineA.line.length();
-        int lastIndexInLine = lineLengthA - 1;
-        int startingIndex = previousNumber.startingIndex - 1;
-        int endingIndex = previousNumber.endingIndex + 1;
-        int diagonalOffset = 2;
-        int i = 0;
-
-        if (previousNumber.endingIndex == lastIndexInLine) {
-          diagonalOffset--;
-        }
-
-        if (previousNumber.startingIndex == 0) {
-          diagonalOffset--;
-          startingIndex = previousNumber.startingIndex;
-        }
-
-        Integer[] possibleVerticalIndeces = new Integer[Integer.toString(previousNumber.value).length()
-            + diagonalOffset];
-
-        while (startingIndex <= endingIndex && i < possibleVerticalIndeces.length) {
-          possibleVerticalIndeces[i] = startingIndex;
-          startingIndex++;
-          i++;
-        }
-
-        for (Symbol currentSymbol : parsedLineB.getSymbolLocations()) {
-          if (currentSymbol.index > endingIndex) {
-            break;
-          }
-          if (Day3Part1.contains(possibleVerticalIndeces, currentSymbol.index) && !previousNumber.isEnginePart()) {
-            sum += previousNumber.value;
-            previousNumber.setIsEnginePart(true);
-          }
-        }
+        inDigit = true;
+        digitBuilder.append(characters[i]);
       }
 
-      return sum;
-    }
-
-    public List<Symbol> parseSymbolLocations() {
-      char[] characters = this.line.toCharArray();
-      List<Symbol> symbolLocations = new ArrayList<>();
-
-      for (int i = 0; i < characters.length; i++) {
-        if (ParsedLine.isSymbol(characters[i])) {
-          symbolLocations.add(new Symbol(characters[i], i));
-        }
-      }
-
-      return symbolLocations;
-    }
-
-    public List<Number> parseNumberLocations() {
-      boolean inDigit = false;
-      int digitStartingIndex = -1;
-      Character previousCharacter = null;
-      char[] characters = this.line.toCharArray();
-      StringBuilder digitBuilder = new StringBuilder("");
-      List<Number> numberLocations = new ArrayList<>();
-
-      for (int i = 0; i < characters.length; i++) {
-        if (Character.isDigit(characters[i])) {
-          if (previousCharacter == null || !Character.isDigit(previousCharacter)) {
-            digitStartingIndex = i;
-          }
-          inDigit = true;
-          digitBuilder.append(characters[i]);
+      if ((!Character.isDigit(characters[i]) || i == characters.length - 1) && inDigit) {
+        int endingIndex = i - 1;
+        if (i == characters.length - 1) {
+          endingIndex = i;
         }
 
-        if ((!Character.isDigit(characters[i]) || i == characters.length - 1) && inDigit) {
-          int endingIndex = i - 1;
-          if (i == characters.length - 1) {
-            endingIndex = i;
-          }
-          Number numberLocation = new Number(Integer.parseInt(digitBuilder.toString()), digitStartingIndex,
-              endingIndex);
-          numberLocations.add(numberLocation);
-          inDigit = false;
-          digitStartingIndex = -1;
-          digitBuilder.delete(0, digitBuilder.length());
+        if (isEnginePartNumber(line, lines, row, digitStartingIndex, endingIndex, line.length() - 1,
+            lines.length - 1)) {
+          partialSum += Integer.parseInt(digitBuilder.toString());
         }
 
-        previousCharacter = characters[i];
+        inDigit = false;
+        digitStartingIndex = -1;
+        digitBuilder.delete(0, digitBuilder.length());
       }
 
-      return numberLocations;
+      previousCharacter = characters[i];
     }
 
-    @Override
-    public boolean equals(Object other) {
-      if (other == null) {
-        return false;
-      }
-      if (ParsedLine.class.isInstance(other)) {
-        ParsedLine otherParsedLine = (ParsedLine) other;
-        if (this.number == otherParsedLine.number && this.line.equals(otherParsedLine.line)) {
-          if (this.numberLocations.size() == otherParsedLine.numberLocations.size()) {
-            if (this.numberLocations.equals(otherParsedLine.numberLocations)) {
-              return true;
-            }
-          }
-        }
-      }
-      return false;
-    }
-
-    static boolean isSymbol(char c) {
-      return !(Character.isDigit(c) || c == '.');
-    }
+    return partialSum;
   }
 
-  public static class Number {
-    public final int value;
-    public final int startingIndex;
-    public final int endingIndex;
-    private boolean enginePart = false;
-
-    public Number(int value, int startingIndex, int endingIndex) {
-      this.value = value;
-      this.startingIndex = startingIndex;
-      this.endingIndex = endingIndex;
-    }
-
-    public boolean isEnginePart() {
-      return this.enginePart;
-    }
-
-    private void setIsEnginePart(boolean isEnginePart) {
-      this.enginePart = isEnginePart;
-    }
-
-    @Override
-    public String toString() {
-      return String.format("Value: %d, Starting Index: %d, Ending Index: %d, Engine Part: %b", this.value,
-          this.startingIndex, this.endingIndex, this.enginePart);
-    }
-
-    @Override
-    public boolean equals(Object other) {
-      if (other == null) {
-        return false;
-      }
-      if (Number.class.isInstance(other)) {
-        Number otherSymbol = (Number) other;
-        if (this.value == otherSymbol.value && this.startingIndex == otherSymbol.startingIndex
-            && this.endingIndex == otherSymbol.endingIndex) {
-          return true;
-        }
-      }
-      return false;
-    }
-  }
-
-  public static class Symbol {
-    public final char value;
-    public final int index;
-
-    public Symbol(char value, int index) {
-      this.value = value;
-      this.index = index;
-    }
-
-    @Override
-    public String toString() {
-      return String.format("Value: %c, Index: %d", this.value, this.index);
-    }
-
-    @Override
-    public boolean equals(Object other) {
-      if (other == null) {
-        return false;
-      }
-      if (Symbol.class.isInstance(other)) {
-        Symbol otherSymbol = (Symbol) other;
-        if (this.value == otherSymbol.value && this.index == otherSymbol.index) {
-          return true;
-        }
-      }
-      return false;
-    }
+  static boolean isSymbol(char c) {
+    return !(Character.isDigit(c) || c == '.');
   }
 }
--- src/com/aoc/tests/day3/Day3Part2Test.java
+++ src/com/aoc/tests/day3/Day3Part2Test.java
@@ -1,6 +1,7 @@
 package com.aoc.tests.day3;
 
 import com.aoc.api.AdventOfCodeApi;
+import com.aoc.day3.Day3Part2;
 import com.aoc.tests.DayTest;
 
 public class Day3Part2Test implements DayTest {
@@ -11,6 +12,17 @@ public class Day3Part2Test implements DayTest {
   }
 
   public void run() {
-    System.out.println("TODO");
+    System.out.println("Running tests for day 3 part 2:");
+    System.out.printf("Test 1: Day 3 part 1 input test: %s\n", day3Part2InputTest() ? "PASSED" : "FAILED");
+    System.out.println();
+  }
+
+  private boolean day3Part2InputTest() {
+    long startTime = System.nanoTime();
+    int result = new Day3Part2(this.api).solve();
+    double duration = Long.valueOf((System.nanoTime() - startTime)).doubleValue() / 1_000_000_000;
+    System.out.printf("Day 3 part 1 optimized took: %f seconds\n", duration);
+    System.out.printf("Test result: %d should equal: %d\n", result, 509115);
+    return 509115 == result;
   }
 }
