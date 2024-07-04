8e6b7ddae1e4ac22938f934143f5e76465371507
Nate Schainblatt <nschainblatt@gmail.com>
2024-07-04 17:00:08 -0400

    Copy day 4 part 1 into day 4 part 2

---
+++ src/com/aoc/day4/Day4Part2.java
@@ -0,0 +1,48 @@
+package com.aoc.day4;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import com.aoc.Day;
+
+public class Day4Part2 implements Day {
+
+  public int solve(String[] input) {
+    int sum = processInput(input);
+    return sum;
+  }
+
+  public static int processInput(String[] input) {
+    int points = 0;
+
+    for (String line : input) {
+      String[] linePart = line.split("\\|");
+      String myNumbersAsString = linePart[1];
+      String winningNumbersAsString = linePart[0].split("\\:")[1];
+      String cardName = linePart[0].split("\\:")[0].trim();
+      points += getWinningPoints(parseLineNumbers(myNumbersAsString), parseLineNumbers(winningNumbersAsString));
+    }
+
+    return points;
+  }
+
+  public static List<Integer> parseLineNumbers(String linePart) {
+    return Arrays.stream(linePart.trim().split(" "))
+        .filter(Predicate.not(String::isEmpty))
+        .mapToInt(s -> Integer.parseInt(s.trim())).boxed().collect(Collectors.toList());
+  }
+
+  private static int getWinningPoints(List<Integer> myNumbers, List<Integer> winningNumbers) {
+    int points = 0;
+
+    for (Integer winningNumber : winningNumbers) {
+      if (myNumbers.contains(winningNumber)) {
+        points = points == 0 ? 1 : points * 2;
+      }
+    }
+
+    return points;
+  }
+}
