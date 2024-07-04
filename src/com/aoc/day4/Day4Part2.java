d6a62dbb63bee1f5a870268490da259f5835b7fc
Nate Schainblatt <nschainblatt@gmail.com>
2024-07-04 17:44:22 -0400

    Update processing to store in ProccessedLine object for easier handling. Update winning count method to count the number of winning numbers per card vs counting points

--- src/com/aoc/day4/Day4Part2.java
+++ src/com/aoc/day4/Day4Part2.java
@@ -1,7 +1,9 @@
 package com.aoc.day4;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -14,18 +16,40 @@ public class Day4Part2 implements Day {
     return sum;
   }
 
+  private static class ProcessedLine {
+    final String cardName;
+    final List<Integer> myNumbers;
+    final List<Integer> winningNumbers;
+
+    ProcessedLine(String cardName, List<Integer> myNumbers, List<Integer> winningNumbers) {
+      this.cardName = cardName;
+      this.myNumbers = myNumbers;
+      this.winningNumbers = winningNumbers;
+    }
+  }
+
+  private static ProcessedLine processLine(String line) {
+    String[] linePart = line.split("\\|");
+    String myNumbersAsString = linePart[1];
+    String winningNumbersAsString = linePart[0].split("\\:")[1];
+    String cardName = linePart[0].split("\\:")[0].trim();
+    return new ProcessedLine(
+        cardName,
+        parseLineNumbers(myNumbersAsString),
+        parseLineNumbers(winningNumbersAsString));
+  }
+
   public static int processInput(String[] input) {
-    int points = 0;
+    int scratchCardCount = 0;
 
     for (String line : input) {
-      String[] linePart = line.split("\\|");
-      String myNumbersAsString = linePart[1];
-      String winningNumbersAsString = linePart[0].split("\\:")[1];
-      String cardName = linePart[0].split("\\:")[0].trim();
-      points += getWinningPoints(parseLineNumbers(myNumbersAsString), parseLineNumbers(winningNumbersAsString));
+      scratchCardCount++; // Original card
+      ProcessedLine processedLine = processLine(line);
+      int numberOfCardsToCopy = getWinningNumberCount(processedLine.myNumbers, processedLine.winningNumbers);
+      // TODO: copy cards and store in hashmap
     }
 
-    return points;
+    return scratchCardCount;
   }
 
   public static List<Integer> parseLineNumbers(String linePart) {
@@ -34,15 +58,15 @@ public class Day4Part2 implements Day {
         .mapToInt(s -> Integer.parseInt(s.trim())).boxed().collect(Collectors.toList());
   }
 
-  private static int getWinningPoints(List<Integer> myNumbers, List<Integer> winningNumbers) {
-    int points = 0;
+  private static int getWinningNumberCount(List<Integer> myNumbers, List<Integer> winningNumbers) {
+    int winningNumberCount = 0;
 
     for (Integer winningNumber : winningNumbers) {
       if (myNumbers.contains(winningNumber)) {
-        points = points == 0 ? 1 : points * 2;
+        winningNumberCount++;
       }
     }
 
-    return points;
+    return winningNumberCount;
   }
 }
