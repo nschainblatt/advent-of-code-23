49a8aac77f144afaeb9bf77e72924471d09310ec
Nate Schainblatt <nschainblatt@gmail.com>
2024-07-08 22:36:09 -0400

    Optimized day 4 part 1 solution

--- src/com/aoc/day4/Day4Part1.java
+++ src/com/aoc/day4/Day4Part1.java
@@ -1,7 +1,9 @@
 package com.aoc.day4;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -21,10 +23,18 @@ public class Day4Part1 implements Day {
       String[] linePart = line.split("\\|");
       String myNumbersAsString = linePart[1];
       String winningNumbersAsString = linePart[0].split("\\:")[1];
-      points += getWinningPoints(parseLineNumbers(myNumbersAsString), parseLineNumbers(winningNumbersAsString));
+      points += getWinningPoints(parseLineNumbers(myNumbersAsString), getWinningDict(parseLineNumbers(winningNumbersAsString)));
     }
 
     return points;
+  }
+
+  public static Map<Integer, Boolean> getWinningDict(List<Integer> winningNumbers) {
+      Map<Integer, Boolean> winningDict = new HashMap<>();
+      for (Integer number : winningNumbers) {
+        winningDict.put(number, true);
+      }
+      return winningDict;
   }
 
   public static List<Integer> parseLineNumbers(String linePart) {
@@ -33,11 +43,11 @@ public class Day4Part1 implements Day {
         .mapToInt(s -> Integer.parseInt(s.trim())).boxed().collect(Collectors.toList());
   }
 
-  private static int getWinningPoints(List<Integer> myNumbers, List<Integer> winningNumbers) {
+  private static int getWinningPoints(List<Integer> myNumbers, Map<Integer, Boolean> winningNumbers) {
     int points = 0;
 
-    for (Integer winningNumber : winningNumbers) {
-      if (myNumbers.contains(winningNumber)) {
+    for (Integer number : myNumbers) {
+      if (winningNumbers.getOrDefault(number, false)) {
         points = points == 0 ? 1 : points * 2;
       }
     }
